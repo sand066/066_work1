29813e51a6fdc25f4c4c88b8956c2f2199796019
sand <nattaphon.sisa@mail.kmutt.ac.th>
2023-03-01 00:10:12 +0700

    add enter password and companyr

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 6 password your company : ");
+            int password = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your company (CIA, FBI, or NSA): ");
+            string company = Console.ReadLine();
+            
+        }
+        
+    }
+}
