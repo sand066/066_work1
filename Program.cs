4589d05b327f8f53abd7a5a6aec42ab86a76602b
sand <nattaphon.sisa@mail.kmutt.ac.th>
2023-03-01 00:30:33 +0700

    finish CIA

--- Program.cs
+++ Program.cs
@@ -11,8 +11,28 @@ namespace PasswordChecker
 
             Console.Write("Enter your company (CIA, FBI, or NSA): ");
             string company = Console.ReadLine();
+            bool answered = false;
             
+            switch (company)
+            {
+                case "CIA":
+
+                    if ((password % 10)%3 == 0 && (password / 10 )%10 !=1 && (password / 10 )%10 !=3 && (password / 10 )%10 !=5
+                    && (password / 1000 )%10 >=6 && (password / 1000 )%10 !=8)
+
+                    {
+                        answered = true;
+                    }
+                    break;
+            }
+        if (answered)
+            {
+                Console.WriteLine("Your password is corrected.");
+            }
+            else
+            {
+                Console.WriteLine("Password is incorrected.");
+            }
         }
-        
     }
 }
